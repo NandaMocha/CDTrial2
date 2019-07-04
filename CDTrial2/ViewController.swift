69c8a9eddb5583c270e56687f238c4e2bf264efb
NandaMochammad <nanda.mocha@gmail.com>
2019-07-04 18:12:05 +0700

    Done

--- CDTrial2/ViewController.swift
+++ CDTrial2/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var namaTF: UITextField!
+    @IBOutlet weak var namaLabel: UILabel!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var namaArray = [Nama]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loadItems()
+
     }
 
+    @IBAction func Add(_ sender: Any) {
+        
+        if let nama = namaTF.text{
+            let data = Nama(context: self.context)
+            data.namaUser = nama
+            
+            namaArray.append(data)
+            saveItems()
+            loadItems()
+        }
+    }
+    
+    func saveItems(){
+        do {
+            try context.save()
+        } catch  {
+            print("error is ", error)
+        }
+        
+        namaTF.text = ""
+        
+    }
+    
+    func loadItems() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Nama")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            namaArray = try context.fetch(request) as! [Nama]
+        } catch  {
+            print("error, ", error)
+        }
+        
+        print("nama array adalah, ", namaArray)
+        
+        namaLabel.text = namaArray.last!.namaUser
 
+    }
+    
 }
 
